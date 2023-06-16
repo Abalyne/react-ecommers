637b69e621000e597a9ef9802eca74903f194a84
Abalyne <67121579+Abalyne@users.noreply.github.com>
2023-06-16 21:19:45 +0100

    adding cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import { CartContext } from "./contexts/CartContext"
 import HeaderIMG from "../assets/Banner_Image.png";
 import styles from "./Header.module.css";
 import { useNavigate } from "react-router-dom";
+import { useHover } from "usehooks-ts"
+import { cartIconClosed } from "react-icons/BsCartDashFill"
 import Cart from "./cart/Cart";
 
 
@@ -11,6 +13,8 @@ const Header = ({ bannerVisibility }) => {
   const [showBanner, setShowBanner] = useState(true);
   const [showModal, setShowModal] = useState(false);
   const nav = useNavigate();
+  const hoverRef = useRef(null)
+  const isHover = useHover(hoverRef)
 
   useEffect(() => {
     setShowBanner(bannerVisibility);
@@ -34,7 +38,7 @@ const Header = ({ bannerVisibility }) => {
       {showBanner && (
         <header className={styles["header-show"]}>
           <h2 onClick={homePageSwitchHandler}>ApeInCables</h2>
-          <button onClick={cartShowModalHandler} />
+          <button onClick={cartShowModalHandler} ref={hoverRef} {() => {return <cartIconClosed />}}/>
         </header>
       )}
       {!showBanner && (
